6f0958bd10ed84cf40dd54a44557d39269341a77
Eli Simhayev <elisimhayev@gmail.com>
2019-04-02 01:12:32 +0300

    refactor expriments

--- assignment1/utils.py
+++ assignment1/utils.py
@@ -1,31 +1,36 @@
 import numpy as np
 import keras
+from sklearn.preprocessing import StandardScaler
 
 
-def print_shapes(x_train, y_train, x_val, y_val, x_test, y_test):
+def print_shapes(x_train, y_train, x_test, y_test):
     print(f"x_train.shape = {x_train.shape}")
     print(f"y_train.shape = {y_train.shape}\n")
     print(f"x_test.shape = {x_test.shape}")
     print(f"y_test.shape = {y_test.shape}\n")
-    print(f"x_val.shape = {x_val.shape}")
-    print(f"y_val.shape = {y_val.shape}\n")
 
 
-def prepare_data(x_train, y_train, x_val, y_val, x_test, y_test):
+def prepare_data(x_train, y_train, x_test, y_test):
     """
     Perform one-hot encoding to the labels, and reshaping to the data
     """
     num_classes = len(np.unique(y_train))
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_val = keras.utils.to_categorical(y_val, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+    y_train = keras.utils.to_categorical(y_train.reshape(-1,1), num_classes)
+    y_test = keras.utils.to_categorical(y_test.reshape(-1,1), num_classes)
     
     image_size = 784
-    X_train = x_train.reshape(-1, image_size)
-    X_test = x_test.reshape(-1, image_size)
-    X_val = x_val.reshape(-1, image_size)
+    x_train = x_train.reshape(-1, image_size)
+    x_test = x_test.reshape(-1, image_size)
     
-    return x_train, y_train, x_val, y_val, x_test, y_test
+    return x_train, y_train, x_test, y_test
+
+
+def scale_data(x_train, x_test):
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+    return x_train, x_test
 
 
 def plot(to_plot, title='Title', xlabel='', ylabel=''):
