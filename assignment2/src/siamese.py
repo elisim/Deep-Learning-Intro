20618f9babeacde138bcfe0a3df0dfd759da5e00
Eli Simhayev <elisimhayev@gmail.com>
2019-05-26 22:59:40 +0300

    added hani acrhi with results

--- assignment2/src/siamese.py
+++ assignment2/src/siamese.py
@@ -7,9 +7,11 @@ KL = keras.layers
 
 class Siamese():
 
-    def __init__(self, lr=1e-3):
+    def __init__(self, lr=1e-4, 
+                       metrics=['binary_accuracy', 'accuracy']):
         self.image_dim = 250
         self.lr = lr
+        self.metrics = metrics
 
     def init_network(self):
         # TODO: add weight init
@@ -21,29 +23,32 @@ class Siamese():
         second_input = KL.Input(input_shape)
 
         model = keras.Sequential()
-        initialize_weights = keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=84)  # filters initialize
+        initialize_weights = keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=84)  # filters initialize
         initialize_bias = keras.initializers.RandomNormal(mean=0.5, stddev=0.01, seed=84)  # bias initialize
 
-        model.add(KL.Conv2D(64, (10, 10), activation='relu', input_shape=input_shape,
+        model.add(KL.Conv2D(5, (6, 6), strides=(2, 2), activation='relu', input_shape=input_shape,
                          kernel_initializer=initialize_weights, kernel_regularizer=l2(1e-2)))
         # model.add(KL.BatchNormalization())
         model.add(KL.MaxPool2D())
 
-        model.add(KL.Conv2D(128, (7, 7), activation='relu', kernel_initializer=initialize_weights,
+        model.add(KL.Conv2D(14, (6, 6), strides=(2, 2), activation='relu', kernel_initializer=initialize_weights,
+                         bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
+        #model.add(KL.BatchNormalization())
+        model.add(KL.MaxPool2D())
+
+        model.add(KL.Conv2D(60, (6, 6), activation='relu', kernel_initializer=initialize_weights,
                          bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
         # model.add(KL.BatchNormalization())
         model.add(KL.MaxPool2D())
 
-        model.add(KL.Conv2D(128, (4, 4), activation='relu', kernel_initializer=initialize_weights,
-                         bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
-        # model.add(KL.BatchNormalization())
-        model.add(KL.MaxPool2D())
-
-        model.add(KL.Conv2D(256, (4, 4), activation='relu', kernel_initializer=initialize_weights,
-                         bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
+        # model.add(KL.Conv2D(256, (4, 4), activation='tanh', kernel_initializer=initialize_weights,
+        #                  bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
         model.add(KL.Flatten())
 
-        model.add(KL.Dense(4096, activation='sigmoid', kernel_regularizer=l2(1e-4),
+        model.add(KL.Dense(40, activation='relu', kernel_regularizer=l2(1e-4),
+                        kernel_initializer=initialize_weights, bias_initializer=initialize_bias))
+
+        model.add(KL.Dense(40, activation=None, kernel_regularizer=l2(1e-4),
                         kernel_initializer=initialize_weights, bias_initializer=initialize_bias))
 
         # Generate the encodings (feature vectors) for the two images
