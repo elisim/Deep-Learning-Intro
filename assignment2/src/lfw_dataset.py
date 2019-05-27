f23b81a3825587058786575ea640a87cb5b50486
Eli Simhayev <elisimhayev@gmail.com>
2019-05-28 00:28:23 +0300

    added vggface transfer learning

--- assignment2/src/lfw_dataset.py
+++ assignment2/src/lfw_dataset.py
@@ -11,6 +11,9 @@ import requests
 from imageio import imread
 import numpy as np
 from tensorflow.python.keras.utils.data_utils import Sequence
+import keras
+import cv2
+
 
 root='../data/lfw2'
 train_info_url = 'http://vis-www.cs.umass.edu/lfw/pairsDevTrain.txt'
@@ -19,7 +22,7 @@ test_info_url = 'http://vis-www.cs.umass.edu/lfw/pairsDevTest.txt'
 np.random.seed(84)
 
 IMAGES_DIM = 250
-
+VGG_IMAGES_DIM = 224
 
 def load_data(val_size=0.2):
     if not (isdir(root) and exists(root)):
@@ -46,14 +49,12 @@ def load_data(val_size=0.2):
 
     return same_train_paths, diff_train_paths, same_val_paths, diff_val_paths, same_test_paths, diff_test_paths
 
-
 def size():
     data_root = pathlib.Path(root)
     all_image_paths = [str(path) for path in list(data_root.glob('*/*'))]
     random.shuffle(all_image_paths)
     return len(all_image_paths)
-    
-    
+        
 def _download():
     dest = root + '.zip'
     print("Started Downloading Labeled Faces in the Wild...")
@@ -65,7 +66,6 @@ def _download():
     remove(dest)
     print("Data downloaded successfully")
     
-
 def _extract_samples_paths(url):
     response = requests.get(url)
     file_text = response.text.split('\n')
@@ -121,6 +121,68 @@ class LFWDataLoader(Sequence):
         for id in image_indexes:
             X[0, index] = _load_image(self.diff_paths[id][0])
             X[1, index] = _load_image(self.diff_paths[id][1])
+            y[index] = 0
+            index += 1
+
+        perm_test = np.random.permutation(y.shape[0])
+        X[0, ] = X[0, perm_test, ]
+        X[1, ] = X[1, perm_test, ]
+        y = y[perm_test]
+
+        return X, y
+
+    def __len__(self):
+        return int(np.floor((len(self.same_paths) + len(self.diff_paths)) / self.batch_size))
+
+    def __getitem__(self, batch_index):
+        images_indexes = self.indexes[batch_index * int(self.batch_size/2):(batch_index + 1) * int(self.batch_size/2)]
+
+        X, y = self.generate_batch(images_indexes)
+        return [X[0,], X[1,]], y
+
+
+def _load_image_vgg(path):
+    gray_image = imread(path)
+    backtorgb = cv2.cvtColor(gray_image,cv2.COLOR_GRAY2RGB)
+    resized_image = cv2.resize(backtorgb, dsize=(VGG_IMAGES_DIM, VGG_IMAGES_DIM), interpolation=cv2.INTER_CUBIC)
+    return resized_image.reshape(1,224,224,3)
+
+
+class LFWDataLoaderVGG(keras.utils.Sequence):
+    def __init__(self, same_paths, diff_paths, batch_size=32, dim=(VGG_IMAGES_DIM, VGG_IMAGES_DIM), shuffle=False):
+        if batch_size % 2 != 0:
+            raise(Exception('batch size need to be dividable by 2'))
+        if len(same_paths) != len(diff_paths):
+            raise(Exception('we should have the same amount of paths for similar images and different images'))
+
+        self.dim = dim
+        self.same_paths = same_paths
+        self.diff_paths = diff_paths
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(self.same_paths))
+
+        self.on_end_of_epoch()
+
+    def on_end_of_epoch(self):
+        self.indexes = np.arange(len(self.same_paths))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def generate_batch(self, image_indexes):
+        X = np.empty((2, self.batch_size, *self.dim, 3), dtype=float)
+        y = np.empty(self.batch_size, dtype=float)
+
+        index = 0
+        for id in image_indexes:
+            X[0, index] = _load_image_vgg(self.same_paths[id][0])
+            X[1, index] = _load_image_vgg(self.same_paths[id][1])
+            y[index] = 1
+            index += 1
+
+        for id in image_indexes:
+            X[0, index] = _load_image_vgg(self.diff_paths[id][0])
+            X[1, index] = _load_image_vgg(self.diff_paths[id][1])
             y[index] = 0
             index += 1
 
--- assignment2/src/siamese.py
+++ assignment2/src/siamese.py
@@ -1,25 +1,35 @@
 import tensorflow as tf
 from keras.regularizers import l2
-from lfw_dataset import LFWDataLoader
+from lfw_dataset import LFWDataLoader, LFWDataLoaderVGG
 
-keras = tf.keras
+import keras
+
+#keras = tf.keras
 K = keras.backend
 KL = keras.layers
 
-
+# for CUBLAS_STATUS_ALLOC_FAILED error
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
 
 class Siamese():
     def __init__(self, lr=1e-4, 
                        momentum=0.9, 
-                       decay=0.99,
-                       metrics=['accuracy']):
+                       decay=0.01,
+                       metrics=['accuracy'],
+                       loss='binary_crossentropy',
+                       batchnorm=False):
         self.image_dim = 250
         self.lr = lr
         self.momentum = momentum
         self.decay = decay
         self.metrics = metrics
+        self.loss = loss
+        self.batchnorm = batchnorm
     
     def build(self, model='hani', model_params=None):
+        self.model_type = model
         model = getattr(self, 'build_' + model)
         if model_params:
             network = model(model_params)
@@ -27,9 +37,11 @@ class Siamese():
             network = model()
         return network
     
+    
     def hyper_hani(self):
         #TODO: implement
         pass
+    
     
     def build_hani(self):
         input_shape = (self.image_dim, self.image_dim, 1)
@@ -42,17 +54,17 @@ class Siamese():
 
         model.add(KL.Conv2D(5, (6, 6), strides=(2, 2), activation='relu', input_shape=input_shape,
                          kernel_initializer=initialize_weights, kernel_regularizer=l2(1e-2)))
-        model.add(KL.BatchNormalization())
+        model = self._add_batchnorm(model)
         model.add(KL.MaxPool2D())
 
         model.add(KL.Conv2D(14, (6, 6), strides=(2, 2), activation='relu', kernel_initializer=initialize_weights,
                          bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
-        model.add(KL.BatchNormalization())
+        
         model.add(KL.MaxPool2D())
 
         model.add(KL.Conv2D(60, (6, 6), activation='relu', kernel_initializer=initialize_weights,
                          bias_initializer=initialize_bias, kernel_regularizer=l2(1e-2)))
-        model.add(KL.BatchNormalization())
+        model = self._add_batchnorm(model)
         model.add(KL.MaxPool2D())
 
         model.add(KL.Flatten())
@@ -68,26 +80,13 @@ class Siamese():
         encoded_r = model(second_input)
 
         # calculate similarity
-        #TODO: add L2 and cosine and euclidian options
         L1_layer = KL.Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))
         L1_distance = L1_layer([encoded_l, encoded_r])
         similarity = KL.Dense(1, activation='sigmoid', bias_initializer=initialize_bias)(L1_distance)
-
-        
-        def contrastive_loss(y_true, y_pred):
-            '''Contrastive loss from Hadsell-et-al.'06
-            http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-            '''
-            margin = 1
-            square_pred = K.square(y_pred)
-            margin_square = K.square(K.maximum(margin - y_pred, 0))
-            return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
-        
         
         final_network = keras.Model(inputs=[first_input, second_input], outputs=similarity)
         optimizer = keras.optimizers.SGD(lr=self.lr, momentum=self.momentum, decay=self.decay)
-        #final_network.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=self.metrics)
-        final_network.compile(loss=contrastive_loss, optimizer=optimizer, metrics=self.metrics)
+        final_network.compile(loss=self.loss, optimizer=optimizer, metrics=self.metrics)
 
         self.model = final_network
         return final_network
@@ -96,16 +95,58 @@ class Siamese():
         pass
     
     def build_vggface(self):
-        pass
+        from keras_vggface.vggface import VGGFace
+    
+        initialize_weights = keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=84)  # filters initialize
+        initialize_bias = keras.initializers.RandomNormal(mean=0.5, stddev=0.01, seed=84)  # bias initialize
+    
+        vggface = VGGFace(model='resnet50')
+        for layer in vggface.layers:
+            layer.trainable = False
         
-    def train(self, same_train_paths, diff_train_paths, same_val_paths, diff_val_paths, batch_size=32, epochs=40, epoch_shuffle=False):
+        input_shape = (224, 224, 3)
+        first_input = KL.Input(input_shape)
+        second_input = KL.Input(input_shape)
         
-        training_generator = LFWDataLoader(same_train_paths, diff_train_paths, shuffle=epoch_shuffle, batch_size=batch_size)
-        validation_generator = LFWDataLoader(same_val_paths, diff_val_paths, shuffle=epoch_shuffle, batch_size=batch_size)
+        first_hidden = vggface(first_input)
+        second_hidden = vggface(second_input)
+
+        L1_layer = KL.Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))
+        L1_distance = L1_layer([first_hidden, second_hidden])
+        similarity = KL.Dense(1, activation='sigmoid', bias_initializer=initialize_bias)(L1_distance)
+
+        final_network = keras.Model(inputs=[first_input, second_input], outputs=similarity)
+        optimizer = keras.optimizers.SGD(lr=self.lr, momentum=self.momentum, decay=self.decay)
+        final_network.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=self.metrics)
+        
+        self.model = final_network
+        return final_network
+
+    def _add_batchnorm(self, model):
+        if self.batchnorm: 
+            model.add(KL.BatchNormalization())
+        return model
+        
+        
+    def train(self, 
+              same_train_paths,       
+              diff_train_paths, 
+              same_val_paths, 
+              diff_val_paths, 
+              batch_size=32, 
+              epochs=40, 
+              epoch_shuffle=False):
+        
+        if self.model_type == 'vggface':
+            training_generator = LFWDataLoaderVGG(same_train_paths, diff_train_paths, shuffle=epoch_shuffle, batch_size=batch_size)
+            validation_generator = LFWDataLoaderVGG(same_val_paths, diff_val_paths, shuffle=epoch_shuffle, batch_size=batch_size)            
+        else:
+            training_generator = LFWDataLoader(same_train_paths, diff_train_paths, shuffle=epoch_shuffle, batch_size=batch_size)
+            validation_generator = LFWDataLoader(same_val_paths, diff_val_paths, shuffle=epoch_shuffle, batch_size=batch_size)
     
         history = self.model.fit_generator(generator=training_generator,
                     validation_data=validation_generator,
-                    use_multiprocessing=True, verbose=1, epochs=epochs)
+                    use_multiprocessing=False, verbose=1, epochs=epochs)
         
         return history
         
